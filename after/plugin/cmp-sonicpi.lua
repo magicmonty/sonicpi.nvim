0b9381dbdfddcc9e0e90fd666f8883de957a954e
Martin Gondermann <magicmonty@pagansoft.de>
2022-03-27 11:46:27 +0200

    Add support for nvim-web-devicons

--- after/plugin/cmp-sonicpi.lua
+++ after/plugin/cmp-sonicpi.lua
@@ -1,1 +1,11 @@
 require('cmp-sonicpi').setup()
+
+local ok, icons = pcall(require, 'nvim-web-devicons')
+if ok then
+  icons.set_icon({
+    ['.sonicpi'] = { icon = '', color = '#FF1493', name = 'SonicPi' },
+  })
+  icons.set_icon({
+    sonicpi = { icon = '', color = '#FF1493', name = 'SonicPi' },
+  })
+end
---
+++ ftdetect/sonicpi.vim
@@ -0,0 +1,1 @@
+autocmd BufNewFile,BufRead *.sonicpi setlocal filetype=sonicpi
